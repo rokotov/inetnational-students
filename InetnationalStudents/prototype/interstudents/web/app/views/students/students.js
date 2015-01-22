0f54e873df1ea59f9f2fb7f2d4de61c61320061b
vkotov <vkotov@26ef6fd7-0f6f-4b14-8892-a5043de72c21>
2015-01-22 12:46:23 +0000

    изменил списки студентов
    git-svn-id: https://subversion.assembla.com/svn/kotov@24 26ef6fd7-0f6f-4b14-8892-a5043de72c21

--- InetnationalStudents/prototype/interstudents/web/app/views/students/students.js
+++ InetnationalStudents/prototype/interstudents/web/app/views/students/students.js
@@ -14,19 +14,15 @@ angular.module('myApp.students', ['ngRoute','ngMaterial'])
 
     .controller('StudentsCtrl', function($scope, $http) {
     	$http.get('http://localhost:8080/is/faculty/all').success(function(data,status,headers,config){
-    		console.log('This is Data:', data,'\n\n This is Status:',status,'\n\n This is Headers:',headers);
     		$scope.faculties = data;
     	});
     	$http.get('http://localhost:8080/is/speciality/all').success(function(data,status,headers,config){
-    		console.log('This is Data:', data,'\n\n This is Status:',status,'\n\n This is Headers:',headers);
     		$scope.specialities = data;
     	});
     	$http.get('http://localhost:8080/is/student/all').success(function(data,status,headers,config){
-    		console.log('This is Data:', data,'\n\n This is Status:',status,'\n\n This is Headers:',headers);
     		$scope.students = data;
     	});
     	$http.get('http://localhost:8080/is/country/all').success(function(data,status,headers,config){
-    		console.log('This is Data:', data,'\n\n This is Status:',status,'\n\n This is Headers:',headers);
     		$scope.countries = data;
     	});
     });    
