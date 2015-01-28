231a2800b08e53dea180de7accb9cf67351c5a8a
vkotov <vkotov@26ef6fd7-0f6f-4b14-8892-a5043de72c21>
2015-01-28 07:50:01 +0000

    Добавил файлы
    git-svn-id: https://subversion.assembla.com/svn/kotov@28 26ef6fd7-0f6f-4b14-8892-a5043de72c21

---
+++ InetnationalStudents/prototype/interstudents/web/app/js/app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by vkotov on 19.01.2015.
+ */
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.login',
+    'myApp.home',
+    'myApp.students',
+    'myApp.version',
+    'ngMaterial'
+]).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/login'});
+    }])
+
+    .controller('MainCtrl', function($scope, $timeout, $mdSidenav) {
+        $scope.toggleRight = function() {
+            $mdSidenav('left').toggle();
+        };
+
+        $scope.menu = {};
+        $scope.menu.pages = [
+            {"url": "/home", "discription":"Главная"},
+            {"url": "/students", "discription":"Список студентов"}
+
+        ];
+
+        $scope.menu.isPageSelected = function(page) {
+            return ($scope.menu.currentPage === page);
+        };
+
+        $scope.menu.toggleSelectPage = function(page) {
+            $scope.menu.currentPage = page;
+        };
+    })
+
+    .controller('LeftCtrl', function($scope, $timeout, $mdSidenav) {
+        $scope.close = function() {
+            $mdSidenav('left').close();
+        };
+    });
